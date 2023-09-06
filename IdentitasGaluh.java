89292db86223479fdf758468eee0d88e650437f9
galuhkanthi14 <galuhkanthi@gmail.com>
2023-09-06 16:35:26 +0700

    prakdaspro tugas kelompok

--- IdentitasGaluh.java
+++ IdentitasGaluh.java
@@ -5,7 +5,7 @@ public class IdentitasGaluh {
 
     public static void main(String[] args) {
         System.out.println("***** IDENTITAS DIRI *****");
-        System.out.println("Nama        : Galuh Puspa K");
+        System.out.println("Nama        : Galuh Puspa Kanthi");
         System.out.println("Absen       : 10");
         System.out.println("Kelas       : TI_1E");
         System.out.println("Prodi       : D-IV Teknik Informatika");
