eb0f6a666235a1fea374d69ea76ba83ae87c3089
galuhkanthi14 <galuhkanthi@gmail.com>
2023-09-06 16:29:31 +0700

    simpan ke java

---
+++ IdentitasGaluh.java
@@ -0,0 +1,14 @@
+/**
+ * IdentitasGaluh
+ */
+public class IdentitasGaluh {
+
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama        : Galuh Puspa Kanthi");
+        System.out.println("Absen       : 10");
+        System.out.println("Kelas       : TI_1E");
+        System.out.println("Prodi       : D-IV Teknik Informatika");
+        System.out.println("Jurusan     : Teknologi Informasi");
+    }
+}
