964396d39422edc21b1338e6d9fa7d2dd54424b9
fawwazalifiofarsa <fawwaz.alifio.farsa@gmail.com>
2023-09-07 09:21:53 +0700

    Testing

---
+++ IdentitasFawwaz.java
@@ -0,0 +1,15 @@
+/**
+ * Tugas
+ */
+public class IdentitasFawwaz {
+    public static void main (String[] args) {
+
+        // Menampilkan output nama
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama        : Fawwaz Alifio Farsa");
+        System.out.println("Absen       : 10");
+        System.out.println("Kelas       : TI-1E");
+        System.out.println("Prodi       : D-IV Teknik Informatika");
+        System.out.println("Jurusan     : Teknologi Informasi");
+    }
+}
